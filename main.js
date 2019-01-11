3062cd4967b2fca10f7e50ba4a63d9022685e189
ilis137 <32433239+ilis137@users.noreply.github.com>
2019-01-11 08:00:47 +0530

    solve bugs and add hover effect and add axis

--- main.js
+++ main.js
@@ -6,29 +6,38 @@ document.addEventListener('DOMContentLoaded', function() {
         var data = JSON.parse(req.responseText)
         var dataset = data.data;
         console.log(dataset[200])
-        var x = d3.scaleBand().domain(dataset.map(function(d) {
-                return d[0]
-            }))
+        var yearsDate = dataset.map(function(d) {
+            return new Date(d[0])
+        })
+        var x = d3.scaleTime().domain([d3.min(yearsDate), d3.max(yearsDate)])
             .range([0, 500])
-            .paddingInner([1])
+
+
+
+        var linearScale = d3.scaleLinear().domain([0, d3.max(dataset, function(d) {
+                return d[1];
+            })])
+            .range([500, 0])
         var y = d3.scaleLinear().domain([0, d3.max(dataset, function(d) {
                 return d[1];
             })])
             .range([0, 500])
 
 
+        var div = d3.select(canvas).append("div").style("opacity", 0).attr("class", "tooltip")
+
         var canvas = d3.select("body")
             .append("svg")
             .attr("x", 300)
-            .attr("width", 500)
-            .attr("height", 500)
+            .attr("width", 600)
+            .attr("height", 600)
 
         canvas.selectAll("rect")
             .data(dataset)
             .enter()
             .append("rect")
-            .attr("x", function(d) { return x(d[0]) })
-            .attr("width", 5)
+            .attr("x", function(d, i) { return x(yearsDate[i]) })
+            .attr("width", 500 / dataset.length)
             .attr("height", function(d) {
                 return y(d[1])
             })
@@ -36,14 +45,30 @@ document.addEventListener('DOMContentLoaded', function() {
                 return 500 - y(d[1])
             })
             .attr("fill", "steelblue")
-
+            .attr("class", "bar")
+            .attr("opacity", 0.9)
+            .attr('transform', 'translate(60, 0)')
+            // .on("mouseover", function(d) {
+            //     div.transition()
+            //         .duration(200)
+            //         .style("opacity", 0.9)
+            //         .style("left", (d3.event.pageX) + "px")
+            //         .style("top", (d3.event.pageY - 28) + "px");
+            // })
+            // .on("mouseout", function(d) {
+            //     div.transition()
+            //         .duration(500)
+            //         .style("opacity", 0);
+            // })
         canvas.append("g")
             .attr("class", "x axis")
-            .attr("transform", "translate(0," + 500 + ")")
+            .attr("transform", "translate(60,500)")
             .call(d3.axisBottom(x))
+
         canvas.append("g")
             .attr("class", "y axis")
-            .call(d3.axisLeft(y));
+            .attr("transform", "translate(60,0)")
+            .call(d3.axisLeft(linearScale));
 
     }
 })
